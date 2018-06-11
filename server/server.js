5de82f3d7f68b4f0c6eb9ec74f624f3834f932f7
HTMLGhozt <Thomas.dillard.studios@gmail.com>
2018-06-11 12:39:23 -0700

    public was in .gitignore

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ const port = process.env.PORT || 5000;
 const server = express();
 server.use(express.json());
 
+var knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : '127.0.0.1',
+    user : 'admin',
+    password : 'admin',
+    database : 'LambdaFace'
+  }
+});
+
 server.listen(port, () => {
   console.info(`Server listening on Port ${port}`);
 });
