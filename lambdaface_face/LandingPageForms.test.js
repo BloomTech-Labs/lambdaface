3ad5e580dff406c1b498640ef9d996322606bcb8
Smithface <kylewibblesmith@gmail.com>
2018-06-18 12:34:06 -0400

    deleted LandingPageForms test
    because LandingPageForms no longer exists

--- lambdaface_face/LandingPageForms.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import LandingPageForms from "./LandingPageForms";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<LandingPageForms />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
