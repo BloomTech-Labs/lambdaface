be21e634db2daac087d2abcef75b283577c4244f
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-06-18 16:51:25 -0400

    Added cors

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true
+}
 
 const routes = require('./api/routes/routes');
 
 const server = express();
 server.use(express.json());
+server.use(cors(corsOptions));
 
 routes(server);
 
