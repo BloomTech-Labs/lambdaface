3388494bc5cc3c55d2546406e1c012bda33fde3d
HTMLGhozt <Thomas.dillard.studios@gmail.com>
2018-06-22 15:00:25 -0700

    fixing seeds

---
+++ server/seeds/00_clear_all.js
@@ -0,0 +1,9 @@
+exports.seed = async (knex) => {
+  // Deletes existing entries from ALL tables
+  await knex('votes').del();
+  await knex('child_comment').del();
+  await knex('comment').del();
+  await knex('post').del();
+  await knex('category').del();
+  await knex('user').del();
+};
--- server/seeds/03_post_seed.js
+++ server/seeds/03_post_seed.js
@@ -1,53 +1,49 @@
 const uuidv4 = require('uuid/v4');
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('post').del()
-    .then(() => {
-      // Inserts seed entries
-      return knex('post').insert([
-        {
-          id: '87925db7-fb1e-4847-b601-b688def0dbca',
-          title: 'Test Title 1',
-          content: 'Content Test 1',
-          userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
-          categoryId: 1,
-        },
-        {
-          id: uuidv4(),
-          title: 'Test Title 2',
-          content: 'Content Test 2',
-          userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
-          categoryId: 2,
-        },
-        {
-          id: uuidv4(),
-          title: 'Test Title 3',
-          content: 'Content Test 3',
-          userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
-          categoryId: 3,
-        },
-        {
-          id: uuidv4(),
-          title: 'Test Title 4',
-          content: 'Content Test 4',
-          userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
-          categoryId: 4,
-        },
-        {
-          id: uuidv4(),
-          title: 'Test Title 5',
-          content: 'Content Test 5',
-          userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
-          categoryId: 5,
-        },
-        {
-          id: uuidv4(),
-          title: 'Test Title 6',
-          content: 'Content Test 6',
-          userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
-          categoryId: 6,
-        },
-      ]);
-    });
+exports.seed = (knex) => {
+  // Inserts seed entries
+  return knex('post').insert([
+    {
+      id: '87925db7-fb1e-4847-b601-b688def0dbca',
+      title: 'Test Title 1',
+      content: 'Content Test 1',
+      userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
+      categoryId: 1,
+    },
+    {
+      id: uuidv4(),
+      title: 'Test Title 2',
+      content: 'Content Test 2',
+      userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
+      categoryId: 2,
+    },
+    {
+      id: uuidv4(),
+      title: 'Test Title 3',
+      content: 'Content Test 3',
+      userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
+      categoryId: 3,
+    },
+    {
+      id: uuidv4(),
+      title: 'Test Title 4',
+      content: 'Content Test 4',
+      userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
+      categoryId: 4,
+    },
+    {
+      id: uuidv4(),
+      title: 'Test Title 5',
+      content: 'Content Test 5',
+      userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
+      categoryId: 5,
+    },
+    {
+      id: uuidv4(),
+      title: 'Test Title 6',
+      content: 'Content Test 6',
+      userId: 'e143939c-f8ef-4737-a168-8c2a1e47eea7',
+      categoryId: 6,
+    },
+  ]);
 };
