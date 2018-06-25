874ca4dbba3752aa085e7fa083e5db248ff1494f
HTMLGhozt <Thomas.dillard.studios@gmail.com>
2018-06-25 09:31:51 -0700

    modified posts to cascade on user delete

--- server/migrations/20180619130314_post.js
+++ server/migrations/20180619130314_post.js
@@ -1,4 +1,3 @@
-
 exports.up = function(knex, Promise) {
   return knex.raw('SET foreign_key_checks = 0;').then(() => {
     return knex.schema.createTable('post', (table) => {
@@ -20,14 +19,16 @@ exports.up = function(knex, Promise) {
 
       table
         .foreign('userId')
-        .references('user.id');
+        .references('user.id')
+        .onDelete('CASCADE');
 
       table
         .integer('categoryId');
 
       table
         .foreign('categoryId')
-        .references('category.id');
+        .references('category.id')
+        .onDelete('CASCADE');
 
       table
         .timestamp('createdAt')
@@ -45,9 +46,8 @@ exports.up = function(knex, Promise) {
         .integer('commentCount')
         .defaultTo(0);
     });
-  })
-  // .finally(() => knex.raw('SET foreign_key_checks = 0;'));
-}
+  });
+};
 
 exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('post');
