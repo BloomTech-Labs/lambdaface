5fbe63fa65e63582f728ce95b4a32732433589cf
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-06-25 12:21:00 -0400

    Switched off local db

--- server/knexfile.js
+++ server/knexfile.js
@@ -15,23 +15,13 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: 'localhost',
-      port: 3306,
-      database: 'lambdaface',
-      user: 'root',
-      password: '12345678'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     }
   },
-  // development: {
-  //   client: 'mysql',
-  //   connection: {
-  //     host: process.env.DB_HOST,
-  //     port: process.env.DB_PORT,
-  //     database: process.env.DB_NAME,
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASSWORD
-  //   }
-  // },
   // todo:
   //  clean up file
   //  set up development
