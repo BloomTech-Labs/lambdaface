2f95d990180d820be79f3dec52c8f30a262c8420
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-06-27 13:33:11 -0400

    Added a basic footer

---
+++ lambdaface_face/src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h1 className="footer__text">
+        Lambda School © 2018
+      </h1>
+    </div>
+  )
+}
+export default Footer;
