f246f0877eaac7c5842639d28d605c05ed382e79
HTMLGhozt <Thomas.dillard.studios@gmail.com>
2018-07-10 13:56:56 -0700

    refactored comment and reply

--- lambdaface_face/src/Components/PostPage/PostPage.js
+++ lambdaface_face/src/Components/PostPage/PostPage.js
@@ -10,70 +10,66 @@ import WriteComment from "./WriteComment";
 // import "../../Styles/PostPage.css";
 import backArrow from "../../Assets/BackArrow.svg";
 
-class PostPage extends React.Component {
+class PostComments extends React.Component {
   state = {
     comments: [],
-    commentsLoaded: false
+    commentsLoaded: false,
   };
 
   componentDidMount() {
     this.getComments();
   }
 
-  getComments = () => {
-    // console.log(this.props.post);
-    const parentId = this.props.post.id;
+  getComments = async () => {
+    const { parentId } = this.props;
+    // TODO: Fix how we increment views
+    await axios.get(`${process.env.REACT_APP_URL}api/post/${parentId}`);
     axios
-      .get(`${process.env.REACT_APP_URL}`.concat(`api/post/${parentId}`))
+      .get(`${process.env.REACT_APP_URL}api/comments/${parentId}`)
       .then(res => {
-        axios
-          .get(`${process.env.REACT_APP_URL}`.concat(`api/comments/${parentId}`))
-          .then(res => {
-            // console.log(res.data);
-            this.setState({ comments: [...res.data], commentsLoaded: true })
-          })
-          .catch(err => {
-            console.error(err);
-          });
+        this.setState({
+          comments: res.data,
+          commentsLoaded: true,
+        });
       })
+      .catch(error => console.error(error));
     // this.setState({ comments: [...testcomments], commentsLoaded: true });
-  };
-
+  }
+  
   render() {
-    const comments = this.state.comments;
-    const commentsLoaded = this.state.commentsLoaded;
-    // console.log("rendered post page");
-    // console.log(this.state.comments);
+    const { comments, commentsLoaded } = this.state;
+    const { parentId } = this.props;
     return (
-      <div className="post-page__container">
-        <div className="post-page__post">
-          <div className="post__left-col">
-            <IconButton onClick={this.props.changeCurrentCategory(this.props.category)}>
-              <img src={backArrow} alt="Back" height="30px" width="30px" />
-            </IconButton>
-          </div>
-
-          <div className="post__right-col">
-            <PostFull post={this.props.post} currentUser={this.props.userInfo.sub} />
-          </div>
-        </div>
-        <div className="post-page__comments">
-          <div className="post-page__comments-header">Comments</div>
-          {commentsLoaded &&
-          comments.map((elem, i) => (
-            <Comment key={elem.id} comment={elem} userInfo={this.props.userInfo} reloadComments={this.getComments} />
-          ))}
-          {/* {!commentsLoaded && <div>Loading Comments...</div>} */}
-          <div className="post-page__new-comment-header">Write a comment</div>
-          <WriteComment
-            commentInfo={{ parentId: this.props.post.id, parentType: 'post' }}
-            userInfo={this.props.userInfo}
-            reloadComments={this.getComments}
-          />
-        </div>
+      <div className="post-page__comments">
+        <div className="post-page__comments-header">Comments</div>
+        {commentsLoaded && comments.map(comment => (
+          <Comment key={comment.id} comment={comment} userInfo={this.props.userInfo} reloadComments={this.getComments} />
+        ))}
+        {/* {!commentsLoaded && <div>Loading Comments...</div>} */}
+        <div className="post-page__new-comment-header">Write a comment</div>
+        <WriteComment
+          commentInfo={{ parentId, parentType: 'post' }}
+          userInfo={this.props.userInfo}
+          reloadComments={this.getComments}
+        />
       </div>
     );
   }
 }
 
-export default PostPage;
+export default props => (
+  <div className="post-page__container">
+    <div className="post-page__post">
+      <div className="post__left-col">
+        <IconButton onClick={props.changeCurrentCategory(props.category)}>
+          <img src={backArrow} alt="Back" height="30px" width="30px" />
+        </IconButton>
+      </div>
+
+      <div className="post__right-col">
+        <PostFull post={props.post} currentUser={props.userInfo.sub} />
+      </div>
+    </div>
+    <PostComments parentId={props.post.id} userInfo={props.userInfo} />
+  </div>
+);
