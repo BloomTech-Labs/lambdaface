03406a68359bae22f8516ba7891baf70af390422
HTMLGhozt <Thomas.dillard.studios@gmail.com>
2018-07-12 12:59:46 -0700

    refactored LandingPage.js to functional component

--- lambdaface_face/src/LandingPage.js
+++ lambdaface_face/src/LandingPage.js
@@ -4,41 +4,37 @@ import HomePage from "./Components/HomePage";
 
 import lambda from './Assets/lambda.svg';
 
-class LandingPage extends React.Component {
-  login = event => {
+
+export default props => {
+  const { isAuthenticated } = props.auth;
+  const login = event => {
     event.preventDefault();
-    this.props.auth.login();
+    props.auth.login();
   };
 
-  logout = event => {
+  const logout = event => {
     event.preventDefault();
-    this.props.auth.logout();
+    props.auth.logout();
   };
-
-  render() {
-    const { isAuthenticated } = this.props.auth;
-    return (
-      <div className="landing-page__container">
-        { isAuthenticated()
-          ? <HomePage logout={this.logout} />
-          : (
-            <div className="landing-page__no-login">
-              <div className="landing-page__topbar">
-                <img src={lambda} alt="LambdaLogo" className="landing-page__logo" />
-              </div>
-              <div className="landing-page__main">
-                <h1>Hello!</h1>
-                <h3>LambdaFace is a private community for Lambda School students.</h3>
-                <Button className="landing-page__login-btn" variant="contained" color="primary" onClick={this.login}>
-                  Login or Register Here
-                </Button>
-              </div>
+  return (
+    <div className="landing-page__container">
+      { isAuthenticated()
+        ? <HomePage logout={logout} />
+        : (
+          <div className="landing-page__no-login">
+            <div className="landing-page__topbar">
+              <img src={lambda} alt="LambdaLogo" className="landing-page__logo" />
             </div>
-          )
-        }
-      </div>
-    );
-  }
+            <div className="landing-page__main">
+              <h1>Hello!</h1>
+              <h3>LambdaFace is a private community for Lambda School students.</h3>
+              <Button className="landing-page__login-btn" variant="contained" color="primary" onClick={login}>
+                Login or Register Here
+              </Button>
+            </div>
+          </div>
+        )
+      }
+    </div>
+  );
 }
-
-export default LandingPage;
